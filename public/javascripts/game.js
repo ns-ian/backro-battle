9a387100af3bc7a17e798d0c83bdaff09dc2b779
Ian Wright <idwright90@gmail.com>
2019-11-10 19:58:52 -0500

    Add basic chat functionality

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,1 +1,15 @@
-var socket = io();
+$(function () {
+  var socket = io();
+
+  $('#chat').submit(function(e) {
+    e.preventDefault();
+    socket.emit('chat message', $('#m').val());
+    $('#m').val('');
+    return false;
+  });
+
+  socket.on('chat message', function(msg) {
+    $('#chat-messages').append($('<li>').text(msg));
+    $('#chat-div').scrollTop($('#chat-div')[0].scrollHeight);
+  });
+});
